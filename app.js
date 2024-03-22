9f5238d74613c42bdc121c47b8edbd3806c4cb63
Daniel Basarab <dantramp52@gmail.com>
2024-03-22 14:56:34 -0700

    add login and recommend buttons

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const inputTitle = document.getElementById('input-title');
 const inputAuthor = document.getElementById('input-author');
 const addBookBtn = document.getElementById('add-book');
 const bookList = document.getElementById('book-list');
+const recommendBtn = document.getElementById('recommend');
 
 addBookBtn.addEventListener('click', addTask);
+
+inputTitle.addEventListener('keypress', handleKeyPress);
+inputAuthor.addEventListener('keypress', handleKeyPress);
+recommendBtn.addEventListener('click', recommendBooks);
 
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
@@ -12,8 +17,16 @@ function handleKeyPress(event) {
     }
 }
 
-inputTitle.addEventListener('keypress', handleKeyPress);
-inputAuthor.addEventListener('keypress', handleKeyPress);
+
+function recommendBooks() {
+    const checkedBooks = document.querySelectorAll('#book-list li.done');
+    checkedBooks.forEach(book => {
+      const title = book.querySelector('span:nth-child(2)').textContent;
+      const author = book.querySelector('.author-text').textContent;
+      console.log(`Title: ${title}, Author: ${author}`);
+    });
+}
+
 function addTask() {
     const bookTitle = inputTitle.value.trim();
     const authorName = inputAuthor.value.trim();
@@ -30,6 +43,12 @@ function addBookItem(title, author) {
     const titleSpan = document.createElement('span');
     const authorSpan = document.createElement('span');
     const deleteBtn = document.createElement('button');
+    const checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', () => {
+        li.classList.toggle('done');
+    });
 
     titleSpan.textContent = title;
     authorSpan.textContent = ` by ${author}`;
@@ -40,10 +59,7 @@ function addBookItem(title, author) {
         bookList.removeChild(li);
     });
 
-    titleSpan.addEventListener('click', () => {
-        li.classList.toggle('done');
-    });
-
+    li.appendChild(checkbox);
     li.appendChild(titleSpan);
     li.appendChild(authorSpan);
     li.appendChild(deleteBtn);
